d76b2b59f98fd3f2bef098ab84ed6535c2fb0a62
Witalij <rujnikov2015@outlook.com>
2021-08-20 22:50:28 +0800

    added docs

--- src/main/java/net/fabricmc/v1rucovsdemons/client/v1ModMainClient.java
+++ src/main/java/net/fabricmc/v1rucovsdemons/client/v1ModMainClient.java
@@ -4,19 +4,19 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.fabricmc.v1rucovsdemons.client.gui.*;
-import net.fabricmc.v1rucovsdemons.main;
+import net.fabricmc.v1rucovsdemons.v1ModMain;
 import net.fabricmc.v1rucovsdemons.common.blockEntity.render.*;
 import net.fabricmc.v1rucovsdemons.common.blockEntity.*;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
-import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 
 public class mainClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient(){
-        ScreenRegistry.<altarGuiDescription, altarScreen>register(main.ALTAR_SCREEN,(gui,inventory, title)->new altarScreen(gui,inventory.player,title));
-        BlockEntityRendererRegistry.INSTANCE.register(main.ALTAR_ENTITY, new BlockEntityRendererFactory<altarEntity>() {
+        ScreenRegistry.<altarGuiDescription, altarScreen>register(v1ModMain.ALTAR_SCREEN,(gui, inventory, title)->new altarScreen(gui,inventory.player,title));
+
+        BlockEntityRendererRegistry.INSTANCE.register(v1ModMain.ALTAR_ENTITY, new BlockEntityRendererFactory<altarEntity>() {
             @Override
             public BlockEntityRenderer<altarEntity> create(Context ctx) {
                 return new altarEntityRenderer(ctx.getRenderDispatcher());
